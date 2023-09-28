6465de704ce60055f7bda4fe312c79b0470a4008
kardell1 <kevindavisilva97@gmail.com>
2023-09-28 15:23:50 -0400

    primer guardado del proyecto

---
+++ tienda-virtual/resources/views/tienda.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- esto es para generar los estilos , por alguna razon no me jala los estilos desde la carpeta css , y los busca dentro de la carpeta public , entonces es mejor dejarlo declarado desde ahi o meterlo en alguna carpeta quizas  -->
+    <link rel="stylesheet" href="{{ asset('estilos.css') }}">
+    <title>Tienda_Virtual</title>
+</head>
+<body>
+    <h1 class="border" >pagina de la tienda</h1>
+    <h2 class="font">otro titulo </h2>
+    <h3 id="prueba" > holaaaaa</h3>
+</body>
+</html>
